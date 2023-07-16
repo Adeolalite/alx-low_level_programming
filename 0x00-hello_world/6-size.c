1d753feddc760c95ec487ab82fe7dd7ace5c93b7
Adeolalite <adeolaalx@gmail.com>
2023-07-16 00:46:44 +0000

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - size of various data types
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+	int e;
+
+	a = sizeof(char);
+	b = sizeof(int);
+	c = sizeof(long);
+	d = sizeof(long long);
+	e = sizeof(float);
+	printf("Size of a char: %d byte(s)\n", a);
+	printf("Size of an int: %d byte(s)\n", b);
+	printf("Size of a long int: %d byte(s)\n", c);
+	printf("Size of a long long int: %d byte(s)\n", d);
+	printf("Size of a float: %d byte(s)\n", e);
+	return (0);
+}
